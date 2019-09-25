ff752f7744caf05f108379673258156c05f4eb58
Zaenal Anzarry <55571352+zaenalanzarry@users.noreply.github.com>
2019-09-25 14:36:19 +0700

    Add files via upload

---
+++ IF1-10118023-Latihan5-KambingGlobal/src/latihan5/Latihan5.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package latihan5;
+
+/**
+ * @author Zaenal PC
+ * NAMA              : Zaenal Anzarry
+ * KELAS             : IF-01
+ * NIM               : 10118023
+ * Deskripsi Program : Kambing Global
+ */
+
+public class Latihan5{
+
+    int jumlahKambing = 88;
+    
+    // Method untuk menampilkan jumlah kambing    
+    public void getJumlahKambing() {
+        System.out.println("Jumlah kambing: " + jumlahKambing);
+    }
+    
+    public void tambahKambing() {
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah kambing setelah ditambah: " + jumlahKambing);
+    }
+    public static void main(String[] args) {
+        Latihan5 kambingSusu = new Latihan5();
+        
+        // Menampilkan jumlah kambing yang ada saat program pertama x berjalan
+        kambingSusu.getJumlahKambing();
+        
+        // Menambah satu kambing
+        kambingSusu.tambahKambing();
+        
+        // Menampilkan jumlah kambing yang ada
+        kambingSusu.getJumlahKambing();
+    }
+    
+}
